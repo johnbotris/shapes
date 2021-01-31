be1651cd022d6af559ad6e3f346e7dc2ac147ec3
John <johnmorris@tuta.io>
2021-01-31 14:34:27 +0100

    midi input base setup, more cli args, better logging, add gag to suppress alsa error output (is there a better way?)

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@
 #![feature(never_type)]
 #![feature(box_syntax)]
 #![feature(destructuring_assignment)]
+#![feature(str_split_once)]
 
-use crate::opt::{getopts, Command, Opts};
 use core::f32::consts::PI;
 use std::{thread, time};
 
 use anyhow::{anyhow, Result};
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use cpal::{Device, Host, Sample, SampleFormat, StreamConfig};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Host, Sample, SampleFormat,
+};
+use midir::{MidiInput, MidiInputPort};
+
+const MIDI_INPUT_NAME: &str = env!("CARGO_PKG_NAME");
 
 mod opts {
-    use anyhow::{anyhow, Result};
+    use anyhow::Result;
     use cpal::{ChannelCount, SampleRate};
     use std::str::FromStr;
     use structopt::StructOpt;
@@ -28,30 +33,42 @@ mod opts {
         #[structopt(short, long, default_value = "48000", parse(try_from_str = parse_sample_rate))]
         pub sample_rate: SampleRate,
 
+        /// Output device to connect to
         #[structopt(short, long, default_value = "pulse")]
         pub device: String,
 
-        #[structopt(subcommand)]
-        pub command: Option<Command>,
+        /// Name of the MIDI input port to connect to
+        #[structopt(short = "p", long)]
+        pub port_name: Option<String>,
 
-        /// Output gain level
+        /// Index of the MIDI input port to connect to
+        #[structopt(short = "i", long)]
+        pub port_index: Option<usize>,
+
+        /// Master gain factor
         #[structopt(short, long, default_value = "0.5")]
         pub gain: f32,
-    }
 
-    #[derive(StructOpt, Debug)]
-    pub enum Command {
-        /// [Default]
-        Run,
+        /// List available audio output devices then exit
+        #[structopt(long)]
+        pub list_outputs: bool,
 
-        ListDevices,
+        /// List available MIDI input ports then exit
+        #[structopt(long)]
+        pub list_inputs: bool,
+
+        /// Output more information, can be passed multiple times
+        #[structopt(short, parse(from_occurrences))]
+        pub verbose: u64,
+
+        /// Output less information, can be passed multiple times
+        #[structopt(short, parse(from_occurrences))]
+        pub quiet: u64,
     }
 
     /// Get and also validate CLI options
     pub fn getopts() -> Opts {
-        let opts = Opts::from_args();
-
-        opts
+        Opts::from_args()
     }
 
     fn parse_sample_rate(input: &str) -> Result<SampleRate> {
@@ -59,60 +76,162 @@ mod opts {
     }
 }
 
-fn main() -> Result<()> {
-    let opts = getopts();
+fn init_logging(opts: &opts::Opts) {
+    use log::LevelFilter::*;
+    use std::cmp::{max, min};
 
-    let host = cpal::default_host();
-
-    match opts.command {
-        Some(Command::Run) | None => run(host, opts)?,
-        Some(Command::ListDevices) => {
-            for (idx, device) in host.output_devices()?.enumerate() {
-                println!(
-                    "{}: {}",
-                    idx,
-                    device.name().unwrap_or("unknown".to_string())
-                );
-            }
-            Ok(())
-        }
+    let default = 3;
+    let verbose = opts.verbose as i64;
+    let quiet = opts.quiet as i64;
+    let level = min(max(default + verbose - quiet, 0), 5);
+    assert!(level <= 5);
+    match simple_logger::SimpleLogger::new()
+        .with_level([Off, Error, Warn, Info, Debug, Trace][level as usize])
+        .init()
+    {
+        Ok(_) => log::trace!("Logging initialized"),
+        Err(e) => eprintln!("Failed to initialize logging: {}", e),
     }
 }
 
-fn run(host: Host, opts: Opts) -> Result<!> {
-    let device = if opts.device == "default" {
-        host.default_output_device()
-    } else {
-        host.output_devices()?
-            .find(|d| d.name().map(|name| name == opts.device).unwrap_or(false))
-    }
-    .ok_or(anyhow!("Couldn't connect to device \"{}\"", opts.device))?;
+fn main() {
+    let opts = opts::getopts();
 
-    println!(
+    let host = cpal::default_host();
+
+    if opts.list_outputs {
+        let devices = host.output_devices();
+        match devices {
+            Ok(devices) => {
+                let _alsa_gag = gag::Gag::stderr().unwrap();
+                println!("Available audio outputs");
+                for (idx, device) in devices.enumerate() {
+                    println!(
+                        "{}: {}",
+                        idx,
+                        device.name().unwrap_or("unknown".to_string())
+                    );
+                }
+            }
+            Err(e) => eprintln!("Error: Unable to enumerate devices: {}", e),
+        };
+    }
+
+    if opts.list_inputs {
+        let input = MidiInput::new(MIDI_INPUT_NAME).unwrap();
+        let ports = input.ports();
+
+        if opts.list_outputs {
+            println!("")
+        }
+        println!("Available MIDI inputs:");
+        for (i, port) in ports.iter().enumerate() {
+            let name = input.port_name(port).unwrap_or(String::from("<unknown>"));
+            if let Some((device, _name)) = name.split_once(':') {
+                println!("{}: {}", i, device);
+            };
+        }
+    }
+
+    if opts.list_inputs || opts.list_outputs {
+        return;
+    }
+
+    init_logging(&opts);
+
+    match run(host, opts) {
+        Ok(_) => {} // unreachable
+        Err(e) => log::error!("Fatal error: {}", e),
+    }
+}
+
+fn run(host: Host, opts: opts::Opts) -> Result<!> {
+    let mut input = MidiInput::new(MIDI_INPUT_NAME)?;
+    input.ignore(midir::Ignore::None);
+    let ports: &[MidiInputPort] = &input.ports();
+    let port = if let Some(name) = &opts.port_name {
+        log::debug!("Connecting to port with name {}", name);
+        ports
+            .iter()
+            .find(|port| input.port_name(port) == Ok(name.clone()))
+            .ok_or(anyhow!("No MIDI port named {}", name))?
+    } else if let Some(index) = opts.port_index {
+        log::debug!("Connecting to port with index {}", index);
+        ports
+            .get(index)
+            .ok_or(anyhow!("Port index {} out of range", index))?
+    } else {
+        log::debug!("Connecting to first available port");
+        match ports {
+            [] => Err(anyhow!("No available MIDI ports")),
+            [port, ..] => Ok(port),
+        }?
+    };
+
+    let port_name = input.port_name(&port).unwrap_or(String::from("<unknown>"));
+
+    let _connection = input
+        .connect(
+            &port,
+            MIDI_INPUT_NAME,
+            move |timestamp, message, _data| {
+                log::trace!(
+                    "Midi input received: timstamp: {}, message: {:?}",
+                    timestamp,
+                    message
+                );
+            },
+            (),
+        )
+        .map_err(|e| {
+            anyhow!(
+                "Couldn't connect to MIDI output port \"{}\": {}",
+                port_name,
+                e
+            )
+        })?;
+
+    log::debug!("Getting midi input from {}", port_name);
+
+    let device = {
+        let _alsa_gag = gag::Gag::stderr().unwrap();
+        if opts.device == "default" {
+            host.default_output_device()
+        } else {
+            host.output_devices()?
+                .find(|d| d.name().map(|name| name == opts.device).unwrap_or(false))
+        }
+        .ok_or(anyhow!("Couldn't connect to device \"{}\"", opts.device))?
+    };
+
+    log::debug!(
         "connected to device: \"{}\"",
         device.name().unwrap_or(String::from("unknown"))
     );
 
-    let supported_config = device
-        .supported_output_configs()?
-        .filter(|config| config.sample_format() == SampleFormat::F32)
-        .filter(|config| {
-            config.min_sample_rate() <= opts.sample_rate
-                && config.max_sample_rate() >= opts.sample_rate
-        })
-        .find(|config| config.channels() == opts.channels)
-        .map(|config| config.with_sample_rate(opts.sample_rate))
-        .or(device.default_output_config().ok())
-        .ok_or(anyhow!("no supported output device config"))?;
+    let supported_config = {
+        let _alsa_gag = gag::Gag::stderr().unwrap();
+        device
+            .supported_output_configs()?
+            .filter(|config| config.sample_format() == SampleFormat::F32)
+            .filter(|config| {
+                config.min_sample_rate() <= opts.sample_rate
+                    && config.max_sample_rate() >= opts.sample_rate
+            })
+            .find(|config| config.channels() == opts.channels)
+            .map(|config| config.with_sample_rate(opts.sample_rate))
+            .or(device.default_output_config().ok())
+            .ok_or(anyhow!("no supported output device config"))?
+    };
 
     let sample_format = supported_config.sample_format();
     let config = supported_config.config();
 
-    println!("channels: {}", config.channels);
-    println!("sample rate: {}", config.sample_rate.0);
-    println!("sample format: {:?}", sample_format);
+    log::info!("channels: {}", config.channels);
+    log::info!("sample rate: {}", config.sample_rate.0);
+    log::info!("sample format: {:?}", sample_format);
 
-    let errfun = |err| eprintln!("an error occurred on the output audio stream: {}", err);
+    let errfun = |err| log::warn!("an error occurred on the output audio stream: {}", err);
     let stream = match sample_format {
         SampleFormat::F32 => device.build_output_stream(
             &config,
@@ -229,8 +348,7 @@ mod vec2 {
 #[cfg(test)]
 mod test {
 
-    use super::vec2::*;
-    use super::*;
+    use super::{vec2::*, *};
 
     #[test]
     fn squares() {
